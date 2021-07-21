8e9963e8ceab747408e0d8ba4d33a2c68fc9cf12
jaydavis359 <jaydavis_1@hotmail.com>
2021-07-21 14:56:57 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $(".tooltipped").tooltip();
+    $('select').formSelect();
     $('.datepicker').datepicker({
         from: "dd mmmm, yyyy",
         yearRange: 3,
