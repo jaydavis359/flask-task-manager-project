b2ad006572107223d2ff21d69c6966942a41a935
jaydavis359 <jaydavis_1@hotmail.com>
2021-07-21 14:14:41 +0000

    Create add_task template

--- app.py
+++ app.py
@@ -96,7 +96,12 @@ def logout():
     # remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
-    return redirect(url_for("login"))    
+    return redirect(url_for("login"))  
+
+
+@app.route("/add_task")
+def add_task():
+    return render_template("add_task.html")      
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
